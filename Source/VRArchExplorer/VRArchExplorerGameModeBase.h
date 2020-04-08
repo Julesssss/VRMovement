a4ab48fd54aad53a89b4e95c8fd8370ef7c4a643
Jules <julianrosser91@gmail.com>
2020-04-08 21:59:28 +0100

    create project

---
+++ Source/VRArchExplorer/VRArchExplorerGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRArchExplorerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRARCHEXPLORER_API AVRArchExplorerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
